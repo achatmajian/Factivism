32836bf7104d2872be06ff75445fbbc66d72a727
Jeremy Gill <jeremygill87@gmail.com>
2020-03-02 20:35:43 -0500

    added components

--- src/App.js
+++ src/App.js
@@ -1,27 +1,28 @@
 import React, { Component } from 'react';
 import './App.css';
-// import Banner from "./components/Banner";
-// import Candidates from "./components/Candidates";
-// import ElectionHome from "./components/ElectionHome";
+import Banner from "./components/Banner";
+import Candidates from "./components/Candidates";
+import ElectionHome from "./components/ElectionHome";
 import Issues from "./components/Issues";
-// import Login from "./components/Login";
-// import Mainpage from "./components/Mainpage";
-// import Sidebar from "./components/Sidebar";
-// import Signup from "./components/Signup";
+import Login from "./components/Login";
+import Mainpage from "./components/Mainpage";
+import Sidebar from "./components/Sidebar";
+import Signup from "./components/Signup";
+import pageData from "./pageData";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Issues/>
-        {/* <Banner pageData={pageData}/>
+        <Banner pageData={pageData}/>
         <Candidates pageData={pageData}/>
         <ElectionHome pageData={pageData}/>
         <Issues pageData={pageData}/>
         <Login pageData={pageData}/>
         <Mainpage pageData={pageData}/>
         <Sidebar pageData={pageData}/>
-        <Signup pageData={pageData}/> */}
+        <Signup pageData={pageData}/>
       </div>
     )
   }
