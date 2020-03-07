a99afcb975f05aa450f34f7dfdc41a87d1774eb1
Villy Devlioti <vadevlio@gmail.com>
2020-03-07 13:49:12 -0500

    form components completed

---
+++ src/components/Form/City.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./City.css";
+
+const City = () => (
+    <div className="form-group city-input">
+        <label for="exampleInputCity">City</label>
+        <input type="text" id="exampleInputCity" placeholder="City"></input>
+    </div>
+  );
+
+  export default City;
---
+++ src/components/Form/Zip.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Zip.css";
+
+const Zip = () => (
+    <div className="form-group zip-input">
+        <label for="exampleInputZip">ZIP Code</label>
+        <input type="text" id="exampleInputZip" placeholder="ZIP"></input>
+    </div>
+  );
+
+  export default Zip;
