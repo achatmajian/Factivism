cadd7ac05332d683219449c3e1f92ebb93780a06
Villy Devlioti <vadevlio@gmail.com>
2020-03-07 13:06:31 -0500

    developing form components

---
+++ src/components/Form/Password.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Password.css";
+
+const Password = () => (
+    <div className="form-group password-input">
+        <label for="exampleInputPassword1">Password</label>
+        <input type="password" id="exampleInputPassword1" placeholder="Password"></input>
+    </div>
+  );
+
+  export default Password;
