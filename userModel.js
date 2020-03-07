5d1f5438fb5e386e93ef5784fd71b9be1cff9ff7
Jeremy Gill <jeremygill87@gmail.com>
2020-03-07 12:11:00 -0500

    attached server to mongo and set up usermodel schema

---
+++ userModel.js
@@ -0,0 +1,54 @@
+var mongoose = require("mongoose");
+
+// Schema constructor
+var Schema = mongoose.Schema;
+
+// create new UserSchema object
+var UserSchema = new Schema({
+    firstname: {
+        type: String,
+        trim: true,
+        required: "firstname is Required"
+    },
+    lastname: {
+        type: String,
+        trim: true,
+        required: "lastname is Required"
+    },
+    streetname: {
+        type: String,
+        trim: true,
+        required: "streetname is Required"
+    },
+    cityname: {
+        type: String,
+        trim: true,
+        required: "cityname is Required"
+    },
+    state: {
+        type: String,
+        trim: true,
+        maxlength: 2,
+        minlength: 2,
+        required: "state is Required"
+    },
+    zipcode: {
+        type: String,
+        trim: true,
+        minlength: 5,
+        maxlength: 5,
+        required: "zipcode is Required"
+    },
+    politicalsurvey: {
+    },
+    // username but it's really the email field
+    username: {
+        type: String,
+        unique: true,
+        match: [/.+@.+\..+/, "Please enter a valid e-mail address"]
+    }
+});
+
+var User = mongoose.model("User", UserSchema);
+
+module.exports = User;
