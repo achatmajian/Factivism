be96a21935b52185da162d42d6b61648e429e009
Villy Devlioti <vadevlio@gmail.com>
2020-03-14 17:28:33 -0400

    housekeeping in components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
 // import Banner from "./components/Banner";
-// import Candidates from "./components/Candidates";
-// import ElectionHome from "./components/ElectionHome";
-//import Issues from "./components/Issues";
-// import Login from "./components/Login";
-// import Mainpage from "./components/Mainpage";
+import Candidates from "./components/Candidates";
+import ElectionHome from "./components/ElectionHome";
+import Issues from "./components/Issues";
+import Login from "./components/Login";
+import Mainpage from "./components/Mainpage";
 // import Sidebar from "./components/Sidebar";
 import Signup1 from "./components/Signup1";
 //import pageData from "./pageData";
@@ -14,7 +14,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-       {/*  <Issues pageData={pageData}/> */}
+       {/*  <Issues pageData={pageData}/> }
         {/* <Banner pageData={pageData}/>
         <Candidates pageData={pageData}/>
         <ElectionHome pageData={pageData}/>
@@ -22,13 +22,22 @@ class App extends Component {
         <Login pageData={pageData}/>
         <Mainpage pageData={pageData}/>
         <Sidebar pageData={pageData}/>
-        <Signup pageData={pageData}/> */
-        <Signup1/> }
+        <Signup pageData={pageData}/> 
+    <Signup1/> */}
       </div>
+
+/*     <Router>
+    <div className="App">
+      <Switch>
+        <Route exact path="/" component={Books} />
+        <Route exact path="/books" component={Books} />
+        <Route exact path="/books/:id" component={Detail} />
+        <Route component={NoMatch} />
+      </Switch>
+    </div>
+  </Router> */
     )
   }
 }
-
-
 
 export default App;
--- src/components/Form/Inputs/Address.js
+++ src/components/Form/Inputs/Address.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Address.css";
 
-const Address = () => (
+const Address = (props) => (
     <div className="form-group address-input">
         <label>Address</label>
         <input type="text" id="exampleInputAddress" placeholder="Address" onChange={props.onChange} />
