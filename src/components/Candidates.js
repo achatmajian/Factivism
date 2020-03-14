00883c6027233a53f470e1a09cd75336f769c426
Jeremy Gill <jeremygill87@gmail.com>
2020-03-14 16:41:25 -0400

    candidates page information is rendering, but needs CSS

--- src/components/Candidates.js
+++ src/components/Candidates.js
@@ -2,15 +2,30 @@ import React, { Component } from "react";
 
 export default class Candidates extends Component {
     render() {
-        let pageData = this.props.pageData.candidates[0];
+        let candidateArrayIndex = 0;
+        let pageData = this.props.pageData.candidates[candidateArrayIndex];
         console.log(pageData);
         return (
             <section id="candidates">
                 <div className="row">
                     <div className="twelve columns collapsed">
                         <h1>{pageData.name}</h1>
-                        <h3>Under the heading name</h3>
-                        <img src="pageData.img_url" className="candidate-img" alt="candidate"/>
+                        <img src={pageData.img_url} className="candidate-img" alt="candidate"/>                        
+                    </div>
+                    <div className="twelve columns-collapsed" id="positions-banner">
+                        <h2>What does {pageData.name} think about...</h2>
+                    </div>
+                    <div className="twelve columns-collapsed">
+                        {pageData.political_positions.map((item, i) => {
+                            return(
+                                <div className="row political-positions-row">
+                                    <ul style={{ listStyleType: "none" }} key={i}>
+                                        <li>{item.position_name}?</li>
+                                        <li>{item.position_stance}</li>
+                                    </ul>
+                                </div>
+                            )
+                        })}
                     </div>
                 </div>
             </section>
