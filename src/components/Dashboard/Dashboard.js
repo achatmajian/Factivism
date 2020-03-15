b3c7177d99fcb65bf6181250f3cd5aad3c05f596
Arek Chatmajian <arek.chatmajian@gmail.com>
2020-03-15 18:58:19 -0400

    added container for candidates on dashboard

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -21,7 +21,12 @@ class Dashboard extends Component {
                 <div className="issues-container">
                     {/* User will click the button in this container to take them to the quiz. */}
                     <h2>Which issues are most important to you? Click here to get the facts on the most prominent issues in American politics today.</h2>
-                    <button type="button" className="button">See Issues</button> 
+                    <button type="button" className="button">Explore Issues</button> 
+                </div>
+                <div className="candidate-container">
+                    {/* User will click the button in this container to take them to the quiz. */}
+                    <h2>With so many candidates in today's political landscape, it can be difficult to wade through and clearly find out a candidate's political stance. Click here to get a brief summary of each candidate.</h2>
+                    <button type="button" className="button">Explore Candidates</button> 
                 </div>
                 
             </section>
