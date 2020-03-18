783338ca50f2e8c10bf74b67c1a6dc094967570a
Arek Chatmajian <arek.chatmajian@gmail.com>
2020-03-18 11:10:00 -0400

    added styling to mainpage

--- src/components/Mainpage/Mainpage.js
+++ src/components/Mainpage/Mainpage.js
@@ -7,12 +7,22 @@ class Mainpage extends Component {
     render() {
         let pageData = this.props.pageData;
         return (
+            <div className="content-container">
             <section id="Mainpage">
-                <h2>Welcome to Factivism</h2>
-                <p>Factivism is an application dedicating to helping people become more informed and civically active! Join today to get information on issues, candidates, and more. Get politically involved with Factivism.</p>
+                <div className="logo-container">
+                <div className="logo">
+                <h2 className="title">Factivism</h2>
+                </div>
+                <p className="subtitle">Factivism is an application dedicating to helping people become more informed and civically active! Join today to get information on issues, candidates, and more. Get politically involved with Factivism.</p>
+                </div>
+
+                <br></br>
+                <div className="button-container">
                 <button type="button" className="button">Log In</button> {/* This button takes the user to the login "page" */}
                 <button type="button" className="button">Sign Up</button> {/* This button takes the user to the sign up 1 "page" */}
+                </div>
             </section>
+            </div>
         )
     }
 }
